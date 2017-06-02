994d8c0f2a540890af1ea1e2c343a0ffeac4c7ca
Oglethorpe Frumplebottom <ofr@dlkrjelr.io>
2017-06-02 07:46:32 -0400

    give circle color; add blackcircle command

--- circle.sh
+++ circle.sh
@@ -2,5 +2,16 @@ function circle()
 {
     local radius=$1 vertical=$2 horizontal=$3
     bumpname
-    cat circle | sed -e "s/RADIUS/$radius/g;s/VERTICAL/$vertical/g;s/HORIZONTAL/$horizontal/g;s/NAME/$NEW_NAME/g" >&3
+    if [ -n "$4" ]
+    then
+	local fill=$4
+    else
+	local fill='#ffffff'
+    fi
+    cat circle | sed -e "s/RADIUS/$radius/g;s/VERTICAL/$vertical/g;s/HORIZONTAL/$horizontal/g;s/FILL/$fill/g;s/NAME/$NEW_NAME/g" >&3
+}
+
+function blackcircle()
+{
+    circle $1 $2 $3 '#000000'
 }
